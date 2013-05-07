b0e3e4fa68981caa5782aac42dc552f38ed75961
Panu Leppäniemi <me@panuleppaniemi.com>
2013-05-07 18:15:07 +0300

    Clean up

--- tests/Xi/Netvisor/Xml/MaterialTest.php
+++ tests/Xi/Netvisor/Xml/MaterialTest.php
@@ -3,34 +3,20 @@
 namespace Xi\Netvisor\Resource;
 
 use Xi\Netvisor\Xml\TestMaterial;
-use JMS\Serializer\Serializer;
-use JMS\Serializer\SerializerBuilder;
 use Xi\Netvisor\XmlTestCase;
 
 class MaterialTest extends XmlTestCase
 {
     /**
-     * @var TestMaterial
-     */
-    private $material;
-
-    public function setUp()
-    {
-        parent::setUp();
-
-        $material = new TestMaterial();
-        $material->attribute = 'attr';
-        $material->value = 'lus';
-
-        $this->material = $material;
-    }
-
-    /**
      * @test
      */
     public function convertsToXml()
     {
-        $xml = $this->toXml($this->material);
+        $material = new TestMaterial();
+        $material->attribute = 'attr';
+        $material->value = 'lus';
+
+        $xml = $this->toXml($material);
 
         $this->assertStringStartsWith('<?xml', $xml);
     }
