32adcc1a489b86d95abe55c5588c0831b12578b1
Panu Leppäniemi <me@panuleppaniemi.com>
2013-05-08 08:32:17 +0300

    Refactor Validate

--- library/Xi/Netvisor/Component/Validate.php
+++ library/Xi/Netvisor/Component/Validate.php
@@ -15,20 +15,30 @@ class Validate
      */
     public function isValid($xml, $filepath)
     {
-        $xml = explode("\n", $xml);
-        $dtd = @file_get_contents($filepath);
-
-        if ($dtd) {
-            $xml[0] .= "\n" . $dtd;
-        }
+        $xml = $this->insertDtd($xml, $filepath);
 
         $dom = new \DOMDocument();
-        $dom->loadXML(implode("\n", $xml));
+        $dom->loadXML($xml);
 
         try {
             return $dom->validate();
         } catch (\Exception $e) {
             return false;
         }
+    }
+
+    /**
+     * @param  string $xml
+     * @param  string $filepath
+     * @return string
+     */
+    private function insertDtd($xml, $filepath)
+    {
+        $dtd = @file_get_contents($filepath);
+
+        $xml = explode("\n", $xml);
+        $xml[0] .= "\n" . $dtd;
+
+        return implode("\n", $xml);
     }
 }
