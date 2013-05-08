380951c544578418687d2aadb1f7f0f23df06333
Panu Leppäniemi <me@panuleppaniemi.com>
2013-05-08 09:29:46 +0300

    Added Config

---
+++ library/Xi/Netvisor/Component/Config.php
@@ -0,0 +1,156 @@
+<?php
+
+namespace Xi\Netvisor\Component;
+
+class Config
+{
+    /**
+     * @var boolean
+     */
+    private $enabled;
+
+    /**
+     * @var string
+     */
+    private $host;
+
+    /**
+     * @var string
+     */
+    private $sender;
+
+    /**
+     * @var string
+     */
+    private $customerId;
+
+    /**
+     * @var string
+     */
+    private $partnerId;
+
+    /**
+     * @var string
+     */
+    private $language;
+
+    /**
+     * @var string
+     */
+    private $organizationId;
+
+    /**
+     * @var string
+     */
+    private $userKey;
+
+    /**
+     * @var string
+     */
+    private $partnerKey;
+
+    /**
+     * @param boolean $enabled
+     * @param string  $host
+     * @param string  $sender
+     * @param string  $customerId
+     * @param string  $partnerId
+     * @param string  $language
+     * @param string  $organizationId
+     * @param string  $userKey
+     * @param string  $partnerKey
+     */
+    public function __construct(
+        $enabled,
+        $host,
+        $sender,
+        $customerId,
+        $partnerId,
+        $language,
+        $organizationId,
+        $userKey,
+        $partnerKey
+    ) {
+        $this->enabled = $enabled;
+        $this->host = $host;
+        $this->sender = $sender;
+        $this->customerId = $customerId;
+        $this->partnerId = $partnerId;
+        $this->language = $language;
+        $this->organizationId = $organizationId;
+        $this->userKey = $userKey;
+        $this->partnerKey = $partnerKey;
+    }
+
+    /**
+     * @return boolean
+     */
+    public function isEnabled()
+    {
+        return $this->enabled;
+    }
+
+    /**
+     * @return string
+     */
+    public function getCustomerId()
+    {
+        return $this->customerId;
+    }
+
+    /**
+     * @return string
+     */
+    public function getHost()
+    {
+        return $this->host;
+    }
+
+    /**
+     * @return string
+     */
+    public function getLanguage()
+    {
+        return $this->language;
+    }
+
+    /**
+     * @return string
+     */
+    public function getOrganizationId()
+    {
+        return $this->organizationId;
+    }
+
+    /**
+     * @return string
+     */
+    public function getPartnerId()
+    {
+        return $this->partnerId;
+    }
+
+    /**
+     * @return string
+     */
+    public function getPartnerKey()
+    {
+        return $this->partnerKey;
+    }
+
+    /**
+     * @return string
+     */
+    public function getSender()
+    {
+        return $this->sender;
+    }
+
+    /**
+     * @return string
+     */
+    public function getUserKey()
+    {
+        return $this->userKey;
+    }
+}
--- library/Xi/Netvisor/Component/Request.php
+++ library/Xi/Netvisor/Component/Request.php
@@ -2,7 +2,14 @@
 
 namespace Xi\Netvisor\Component;
 
+use Guzzle\Http\Client;
+
 class Request
 {
+    private $client;
 
+    public function __construct(Client $client)
+    {
+        $this->client = $client;
+    }
 }
